28468d0f9562463f30a514cd23439053f3e4d3e6
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-19 05:12:57 +0000

    Player class created

--- math_game.rb
+++ math_game.rb
@@ -1,20 +1,20 @@
-# class player
-#   attr_accessor :lives, :score
-#   attr_reader :name
+class Player
+  attr_accessor :lives, :score
+  attr_reader :name
 
-#   def initialize(name, lives)
-#     @name = name
-#     @lives = lives
-#   end
+  def initialize(name, lives)
+    @name = name
+    @lives = lives
+  end
 
-#   def solve(answer)
-#     answer
-#   end
+  def solve(answer)
+    answer
+  end
 
-#   def lose_life
-#     @lives -= 1
-#   end
-# end
+  def lose_life
+    @lives -= 1
+  end
+end
 
 class Game
   def initialize(players)
@@ -26,27 +26,27 @@ class Game
   def question
     a = rand(1..20)
     b = rand(1..20)
-    puts "What does #{a} plus #{b} equal?"
+    puts "#{@players[@currentPlayer].name}, what does #{a} plus #{b} equal?"
     answer = gets.chomp.to_i
 
     if answer == (a + b)
       puts "YEP! #{answer} is CORRECT! :3"
     else
-      @players[@currentPlayer][:lives] -= 1
+      @players[@currentPlayer].lives -= 1
       puts "#{answer} is WRONG! xD The answer is #{a + b}!"
     end
   end
 
   def turn
-    if @players[0][:lives] == 0
+    if @players[0].lives == 0
       game_end @players[1]
       @inSession = false
-    elsif @players[1][:lives] == 0
+    elsif @players[1].lives == 0
       game_end @players[0]
       @inSession = false
     else
       puts question
-      puts "P1: #{@players[0][:lives]}/3 vs P2: #{@players[1][:lives]}/3"
+      puts "P1: #{@players[0].lives}/3 vs P2: #{@players[1].lives}/3"
       puts "--------NEW TURN--------"
       if @currentPlayer == 0
         @currentPlayer = 1
@@ -63,12 +63,15 @@ class Game
   end
 
   def game_end (winner)
-    puts "The winner is -#{winner[:name]}- with a score of #{winner[:lives]}/3"
+    puts "The winner is -#{winner.name}- with a score of #{winner.lives}/3"
     puts "---X-GAME-x-OVER-X----"
     puts "Buh bye! :3"
   end
 
 end
 
-mathOff = Game.new([{ name: "Chuck", lives: 3 }, { name: "David", lives: 3 }])
+player_1 = Player.new("Player 1", 3 )
+player_2 = Player.new("Player 2", 3 )
+
+mathOff = Game.new([player_1, player_2])
 mathOff.game_loop
